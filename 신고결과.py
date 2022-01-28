750bb3d0abba59edfe557e4e9b14be38a786fdf0
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-01-28 16:42:49 +0900

    1 commit

---
+++ 신고결과.py
@@ -0,0 +1,28 @@
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
+k = 2
+# result=[2,1,1,0]
+
+
+def solution(id_list, report, k):
+    data = {}
+    repData = []
+    ans = []
+    for i in id_list:
+        data[i] = [0, []]
+
+    report = set(report)
+    for j in report:
+        j = j.split()
+        data[j[1]][1].append(j[0])
+        if len(data[j[1]][1]) >= k:
+            repData.append(j[1])
+
+    for i in repData:
+        ans = ans + data[i][1]
+
+    answer = list(map(lambda x: ans.count(x), id_list))
+    return answer
+
+
+print(solution(id_list, report, k))
