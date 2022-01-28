7f4d263e00af311b158c9190141de94607a37e93
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-01-28 19:36:07 +0900

    2 commit

--- 비밀지도.py
+++ 비밀지도.py
@@ -1,5 +1,5 @@
 import re
-arr1 = [9, 20, 28, 18, 11]
+arr1 = [0, 0, 0, 0, 0]
 arr2 = [30, 1, 21, 17, 28]
 n = 5
 # 출력	["#####","# # #", "### #", "# ##", "#####"]
@@ -12,7 +12,7 @@ def solution(n, arr1, arr2):
         a = format((arr1[i] | arr2[i]), 'b')
         a = a.replace('1', '#')
         a = a.replace('0', ' ')
-        if len(a) < n:
+        while len(a) < n:
             a = ' '+a
         answer.append(a)
 
