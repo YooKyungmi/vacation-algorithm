635e8de7d543c59ccc533cb555f30138eb96cff7
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-02-08 19:11:55 +0900

    1 commit

---
+++ 인형뽑기.py
@@ -0,0 +1,27 @@
+board = [[0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 3],
+         [0, 2, 5, 0, 1],
+         [4, 2, 4, 4, 2],
+         [3, 5, 1, 3, 1]]
+moves = [1, 5, 3, 5, 1, 2, 1, 4]
+
+
+def solution(board, moves):
+    result = []
+    answer = 0
+    for j in moves:
+        for i in range(4, -1, -1):
+            if board[i-1][j-1] == 0:
+                if board[i][j-1] != 0:
+                    result.append(board[i][j-1])
+                    board[i][j-1] = 0
+                break
+        print(result)
+        if len(result) >= 2:
+            if result[-1] == result[-2]:
+                del result[-2:]
+                answer += 2
+    return answer
+
+
+print(solution(board, moves))
